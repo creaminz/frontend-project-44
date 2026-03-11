364f1012af3c25df5695225e03a6d78b3979dc6a
creaminz <hellrey678@gmail.com>
2026-03-11 14:03:54 +0300

    add changes to src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
+import readlineSync from 'readline-sync'
+
 const greetANdGetUserName = () => {
     var userName = readlineSync.question('May I have your name?');
     console.log(`Hello, ${userName}`);
