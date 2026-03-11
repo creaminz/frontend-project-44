707864be3dd233c76b708c4bc9d612bb5292eed8
creaminz <hellrey678@gmail.com>
2026-03-11 13:47:58 +0300

    create the function greetANdUserName in src/cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 import readlineSync from 'readlineSync'
 
 console.log('Welcome to the Brain Games!')
+
+var userName = readlineSync.question('May I have your name?');
+conaole.log(`Hello, ${userName}`);
---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+
+const greetANdGetUserName = () => {
+    var userName = readlineSync.question('May I have your name?');
+    console.log(`Hello, ${userName}`);
+    return userName;
+}
+
+export default greetANdGetUserName;
