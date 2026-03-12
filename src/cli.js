232475e903677ece8e81b8c4b03da92bb0cb4695
creaminz <hellrey678@gmail.com>
2026-03-12 15:33:02 +0300

    4

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync'
 
 const greetANdGetUserName = () => {
-    var userName = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${userName}`);
-    return userName;
+  var userName = readlineSync.question('May I have your name?')
+  console.log(`Hello, ${userName}`)
+  return userName
 }
 
-export default greetANdGetUserName;
+export default greetANdGetUserName
