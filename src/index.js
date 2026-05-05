8ca1452b6f800b8dac03057500a4660c5d0c3031
root <root@DESKTOP-OMHR8MT.localdomain>
2026-05-05 13:22:02 +0300

    push #5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import readlineSync from 'readline-sync';
 
 const runEngine = (description, getRoundData) => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
+  console.log('May I have your name?');
+  const userName = readlineSync.question(' ')
   console.log(`Hello, ${userName}!`);
   console.log(description);
 
